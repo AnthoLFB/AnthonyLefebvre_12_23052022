9722cd792be5d478a21ae089c72135cde5fa3ef6
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-05-21 15:35:47 +0200

    Initialisation du projet. Création des dossiers et fichiers de base.

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+//React
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+//Pages
+import Home from './pages/Home';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Home/>
+  </React.StrictMode>
+);
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+//React
+import React from 'react';
+
+//Composants
+
+function Home() {
+  return (
+    <React.Fragment>
+        <h1>titre</h1>
+    </React.Fragment>
+  );
+}
+
+export default Home;
