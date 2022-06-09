0da549172f64e71aefadd2d82a1f393b00d97118
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-06-09 15:46:34 +0200

    Modification du lien pour accéder aux données

--- src/repositories/inMemory/user_repository.js
+++ src/repositories/inMemory/user_repository.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-class UserRepository
+class inMemoryUserRepository
 {
     getUserById(id)
     {
-        return axios.get("http://localhost:3000/fixtures/store.json")
+        return axios.get("http://localhost:3000/fixtures/userPersonalData.json")
         .catch(function (error) {console.log("Une erreur est survenue lors de la récupération des données de l'utilisateur ayant l'id " + id + ". Message de l'erreur : " + error)});
     }
 }
 
-export default UserRepository;
+export default inMemoryUserRepository;
