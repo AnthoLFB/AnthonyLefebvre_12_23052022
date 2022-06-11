8b8900376bcea6fffb770ea43424705df3daa989
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-06-11 15:15:11 +0200

    Ajout de la fonction permettant de récupérer les données en local

--- src/repositories/inMemory/user_repository.js
+++ src/repositories/inMemory/user_repository.js
@@ -7,6 +7,12 @@ class inMemoryUserRepository
         return axios.get("http://localhost:3000/fixtures/userPersonalData.json")
         .catch(function (error) {console.log("Une erreur est survenue lors de la récupération des données de l'utilisateur ayant l'id " + id + ". Message de l'erreur : " + error)});
     }
+
+    getUserActivity(id)
+    {
+        return axios.get("http://localhost:3000/fixtures/userActivity.json")
+        .catch(function (error) {console.log("Une erreur est survenue lors de la récupération des données concernant l'activité de l'utilisateur ayant l'id " + id + ". Message de l'erreur : " + error)});
+    }
 }
 
 export default inMemoryUserRepository;
