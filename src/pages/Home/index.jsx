b04ad51c0768c6fe23be1e4c5a0a4b567f4475a6
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-06-12 15:53:45 +0200

    Appel du composant, récupération des données

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,11 +9,16 @@ import DailyActivityChart from '../../components/DailyActivityChart';
 
 //Classes
 import {createUserRepository} from '../../repositories/repository_factory'
+import AverageSessionTimeChart from '../../components/AverageSessionTimeChart';
+
+//Css
+import "../../styles/page/Home.css";
 
 function Home() {
 
   const [userPersonalData, setUserPersonalData] = useState({});
   const [userActivity, setUserActivity] = useState({});
+  const [userAverageSessionTime, setUserAverageSessionTime] = useState({});
   const [isDataLoading, setStatement] = useState(false); 
 
   useEffect(() => {
@@ -23,10 +28,11 @@ function Home() {
     const userRepository = createUserRepository();
     
     
-    Promise.all([userRepository.getUserById(12), userRepository.getUserActivity(12)])
+    Promise.all([userRepository.getUserById(12), userRepository.getUserActivity(12), userRepository.getUserAverageTimeSession(12)])
     .then((dataReceived) => {
       setUserPersonalData(dataReceived[0].data.USER_MAIN_DATA);
-      setUserActivity(dataReceived[1].data.USER_ACTIVITY);
+      setUserActivity(dataReceived[1].data.USER_ACTIVITY.sessions);
+      setUserAverageSessionTime(dataReceived[2].data.USER_AVERAGE_SESSIONS.sessions);
     })
     .then(() => {setStatement(true)});
   }, []);
@@ -45,7 +51,9 @@ function Home() {
     );
   }
 
+  console.log(userPersonalData);
   console.log(userActivity);
+  console.log(userAverageSessionTime);
 
   //Once the data has been loaded, then the content can be displayed
   return (
@@ -54,7 +62,12 @@ function Home() {
         <SidewaysNavbar/>
         <main>
           <UserIdentity name={userPersonalData.userInfos.firstName}/>
-          <DailyActivityChart/>
+          <section className='main__data'>
+            <section className='main__data__charts'>
+              <DailyActivityChart/>
+              <AverageSessionTimeChart sessionsInformation={userAverageSessionTime}/>
+            </section>
+          </section>
         </main>
     </React.Fragment>
   );
