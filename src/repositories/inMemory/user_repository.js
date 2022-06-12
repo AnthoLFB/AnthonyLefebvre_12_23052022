2d2f96941fcb6ae736dd0dcec3b01388e50ca6da
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-06-12 15:54:24 +0200

    Création de la fonction permettant de récupérer les données

--- src/repositories/inMemory/user_repository.js
+++ src/repositories/inMemory/user_repository.js
@@ -13,6 +13,12 @@ class inMemoryUserRepository
         return axios.get("http://localhost:3000/fixtures/userActivity.json")
         .catch(function (error) {console.log("Une erreur est survenue lors de la récupération des données concernant l'activité de l'utilisateur ayant l'id " + id + ". Message de l'erreur : " + error)});
     }
+
+    getUserAverageTimeSession(id)
+    {
+        return axios.get("http://localhost:3000/fixtures/averageSessionTime.json")
+        .catch(function (error) {console.log("Une erreur est survenue lors de la récupération des données concernant la moyenne de temps des sessions de l'utilisateur ayant l'id " + id + ". Message de l'erreur : " + error)});
+    }
 }
 
 export default inMemoryUserRepository;
