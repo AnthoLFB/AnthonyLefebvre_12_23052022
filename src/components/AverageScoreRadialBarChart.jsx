721f191a539649a14735354472616131f6a89af4
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-06-16 16:09:12 +0200

    Création du composant, import de rechart et création du graphique

---
+++ src/components/AverageScoreRadialBarChart.jsx
@@ -0,0 +1,43 @@
+//Recharts
+import { PieChart, Pie } from "recharts";
+
+//CSS
+import "../styles/components/AverageScoreRadialBarChart.css"
+
+function AverageScoreRadialBarChart({userScore})
+{
+    let score = 0;
+
+    if(userScore.todayScore === "undefined")
+    {
+        score = userScore.score
+    }
+
+    score = userScore.todayScore;
+
+    let userValue = score;
+    let maxValue = 1 - score;
+
+    const data = 
+    [
+      { name: "userValue", value: userValue, fill: "#FF0000" },
+      { name: "valueMax", value: maxValue, fill: "#FFFFFF" }
+    ];
+    
+    return (
+        <section className="score-chart-container">
+            <h1 className="score-chart-container__title">Score</h1>
+            <div className="score-chart-container__result">
+              <p className="score-chart-container__result__text"><span className="score-chart-container__result__score">{score * 100}%</span> de votre objectif</p>
+            </div>
+            <PieChart width={320} height={320}>
+              <Pie data={data} dataKey="value" cornerRadius={40} innerRadius={80} outerRadius={90} startAngle={230} endAngle={-230}/>
+            </PieChart>
+        </section>
+      );
+
+
+    
+}
+
+export default AverageScoreRadialBarChart
