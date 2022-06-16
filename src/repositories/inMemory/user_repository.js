1288aa138fabf751fa538f74feb8bd744158d02f
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-06-16 16:10:42 +0200

    Suppression des localhost:3000 dans les liens

--- src/repositories/inMemory/user_repository.js
+++ src/repositories/inMemory/user_repository.js
@@ -4,25 +4,25 @@ class inMemoryUserRepository
 {
     getUserById(id)
     {
-        return axios.get("http://localhost:3000/fixtures/userPersonalData.json")
+        return axios.get("fixtures/userPersonalData.json")
         .catch(function (error) {console.log("Une erreur est survenue lors de la récupération des données de l'utilisateur ayant l'id " + id + ". Message de l'erreur : " + error)});
     }
 
     getUserActivity(id)
     {
-        return axios.get("http://localhost:3000/fixtures/userActivity.json")
+        return axios.get("fixtures/userActivity.json")
         .catch(function (error) {console.log("Une erreur est survenue lors de la récupération des données concernant l'activité de l'utilisateur ayant l'id " + id + ". Message de l'erreur : " + error)});
     }
 
     getUserAverageTimeSession(id)
     {
-        return axios.get("http://localhost:3000/fixtures/averageSessionTime.json")
+        return axios.get("fixtures/averageSessionTime.json")
         .catch(function (error) {console.log("Une erreur est survenue lors de la récupération des données concernant la moyenne de temps des sessions de l'utilisateur ayant l'id " + id + ". Message de l'erreur : " + error)});
     }
 
     getUserActivityType(id)
     {
-        return axios.get("http://localhost:3000/fixtures/userActivityType.json")
+        return axios.get("fixtures/userActivityType.json")
         .catch(function (error) {console.log("Une erreur est survenue lors de la récupération des données concernant les activités de l'utilisateur ayant l'id " + id + ". Message de l'erreur : " + error)});
     }
 }
