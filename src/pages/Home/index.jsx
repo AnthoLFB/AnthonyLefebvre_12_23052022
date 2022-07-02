0721815153a6b98341d991a59191ec47ffeb1241
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-07-02 18:45:29 +0200

    Déplacement du contenu de la page Accueil à Profil. Mise en place de liens de test rapide sur la page d'accueil

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,101 +1,35 @@
 //React
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-//Composants
+//Links
+import { NavLink } from "react-router-dom";
+
+//Components
 import Header from '../../components/Header';
 import SidewaysNavbar from '../../components/SidewaysNavbar';
-import UserIdentity from '../../components/UserIdentity';
-import DailyActivityChart from '../../components/DailyActivityChart';
-import AverageSessionTimeChart from '../../components/AverageSessionTimeChart';
-import ActivityTypeRadarChart from '../../components/ActivityTypeRadarChart';
-import AverageScoreRadialBarChart from '../../components/AverageScoreRadialBarChart';
-import Card from '../../components/Card';
-
-//Classes
-import {createUserRepository} from '../../repositories/repository_factory';
-
-//Images
-import flame from "../../assets/images/flame.svg";
-import chicken from "../../assets/images/chicken.svg"
-import apple from "../../assets/images/apple.svg"
-import cheeseburger from "../../assets/images/cheeseburger.svg"
 
 //Css
 import "../../styles/page/Home.css";
 
 /**
- * React component that displays the content of the page Home.
- * @asynch
+ * React component that displays the content of the page Setting.
  * @returns {HTMLElement} return the content of the page.
  */
 function Home() {
-
-  //Creation of constants to retrieve data via promises
-  const [userPersonalData, setUserPersonalData] = useState({});
-  const [userActivity, setUserActivity] = useState({});
-  const [userAverageSessionTime, setUserAverageSessionTime] = useState({});
-  const [userActivityType, setUserActivityType] = useState({});
-  const [isDataLoading, setStatement] = useState(false); 
-
-  useEffect(() => {
-
-    const userId = 12
-
-    //Recovery of personal data about the user
-    const userRepository = createUserRepository();
-    
-    //We get all the data before displaying them
-    Promise.all([userRepository.getUserById(userId), userRepository.getUserActivity(userId), userRepository.getUserAverageTimeSession(userId), userRepository.getUserActivityType(userId)])
-    .then((dataReceived) => {
-      setUserPersonalData(dataReceived[0].data.data);
-      setUserActivity(dataReceived[1].data.data.sessions);
-      setUserAverageSessionTime(dataReceived[2].data.data.sessions);
-      setUserActivityType(dataReceived[3].data.data);
-    })
-    .then(() => {setStatement(true)});
-  }, []);
-
-  //As long as the data are not loaded, a loader is displayed on the screen
-  if(!isDataLoading)
-  {
-    return (
-      <React.Fragment>
-          <Header/>
-          <SidewaysNavbar/>
-          <main>
-            <p>Loading ...</p>
-          </main>
-      </React.Fragment>
-    );
-  }
-
-  //Once the data has been loaded, then the content can be displayed
   return (
     <React.Fragment>
-        <Header/>
-        <SidewaysNavbar/>
-        <main>
-          <UserIdentity name={userPersonalData.userInfos.firstName}/>
-          
-          <section className='main__data'>
-            <section className='main__data__charts'>
-              <DailyActivityChart userActivity={userActivity}/>
-              <div className='main__data__charts__group'>
-                <AverageSessionTimeChart sessionsInformation={userAverageSessionTime}/>
-                <ActivityTypeRadarChart activityInformation={userActivityType}/>
-                <AverageScoreRadialBarChart userScore={userPersonalData}/>
-              </div>
-            </section>
-            <section className='main__data__cards'>
-              <Card color={"red"} icon={flame} type={"Calories"} value={userPersonalData.keyData.calorieCount} unitOfMeasure={"kCal"}/>
-              <Card color={"blue"} icon={chicken} type={"Proteines"} value={userPersonalData.keyData.proteinCount} unitOfMeasure={"g"}/>
-              <Card color={"yellow"} icon={apple} type={"Glucides"} value={userPersonalData.keyData.carbohydrateCount} unitOfMeasure={"g"}/>
-              <Card color={"pink"} icon={cheeseburger} type={"Lipides"} value={userPersonalData.keyData.lipidCount} unitOfMeasure={"g"}/>
-            </section>
-          </section>
-        </main>
+       <Header/>
+       <SidewaysNavbar/>
+       <main>
+          <h1 className='title'>Liens utiles - debbug</h1>
+          <ul className="test-links-list">
+              <li className="test-links-list__item"><NavLink className="test-links-list__item__link" to="/profil/12">TEST - Utilisateur id n°12</NavLink></li>
+              <li className="test-links-list__item"><NavLink className="test-links-list__item__link" to="/profil/18">TEST - Utilisateur id n°18</NavLink></li>
+          </ul>
+       </main>
     </React.Fragment>
   );
 }
 
 export default Home;
+
--- src/pages/Profil/index.jsx
+++ src/pages/Profil/index.jsx
@@ -1,19 +1,103 @@
 //React
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+//ReactRouter
+import { useParams } from 'react-router-dom'
 
 //Composants
 import Header from '../../components/Header';
 import SidewaysNavbar from '../../components/SidewaysNavbar';
+import UserIdentity from '../../components/UserIdentity';
+import DailyActivityChart from '../../components/DailyActivityChart';
+import AverageSessionTimeChart from '../../components/AverageSessionTimeChart';
+import ActivityTypeRadarChart from '../../components/ActivityTypeRadarChart';
+import AverageScoreRadialBarChart from '../../components/AverageScoreRadialBarChart';
+import Card from '../../components/Card';
+
+//Classes
+import {createUserRepository} from '../../repositories/repository_factory';
+
+//Images
+import flame from "../../assets/images/flame.svg";
+import chicken from "../../assets/images/chicken.svg"
+import apple from "../../assets/images/apple.svg"
+import cheeseburger from "../../assets/images/cheeseburger.svg"
+
+//Css
+import "../../styles/page/Profil.css";
 
 /**
- * React component that displays the content of the page Profil.
+ * React component that displays the content of the page Home.
+ * @asynch
  * @returns {HTMLElement} return the content of the page.
  */
 function Profil() {
+
+  //User's Id
+  const { userId } = useParams();
+
+  //Creation of constants to retrieve data via promises
+  const [userPersonalData, setUserPersonalData] = useState({});
+  const [userActivity, setUserActivity] = useState({});
+  const [userAverageSessionTime, setUserAverageSessionTime] = useState({});
+  const [userActivityType, setUserActivityType] = useState({});
+  const [isDataLoading, setStatement] = useState(false); 
+
+  useEffect(() => {
+
+    //Recovery of personal data about the user
+    const userRepository = createUserRepository();
+    
+    //We get all the data before displaying them
+    Promise.all([userRepository.getUserById(userId), userRepository.getUserActivity(userId), userRepository.getUserAverageTimeSession(userId), userRepository.getUserActivityType(userId)])
+    .then((dataReceived) => {
+      setUserPersonalData(dataReceived[0].data.data);
+      setUserActivity(dataReceived[1].data.data.sessions);
+      setUserAverageSessionTime(dataReceived[2].data.data.sessions);
+      setUserActivityType(dataReceived[3].data.data);
+    })
+    .then(() => {setStatement(true)});
+  }, [userId]);
+
+  //As long as the data are not loaded, a loader is displayed on the screen
+  if(!isDataLoading)
+  {
+    return (
+      <React.Fragment>
+          <Header/>
+          <SidewaysNavbar/>
+          <main>
+            <p>Loading ...</p>
+          </main>
+      </React.Fragment>
+    );
+  }
+
+  //Once the data has been loaded, then the content can be displayed
   return (
     <React.Fragment>
-       <Header/>
-       <SidewaysNavbar/>
+        <Header/>
+        <SidewaysNavbar/>
+        <main>
+          <UserIdentity name={userPersonalData.userInfos.firstName}/>
+          
+          <section className='main__data'>
+            <section className='main__data__charts'>
+              <DailyActivityChart userActivity={userActivity}/>
+              <div className='main__data__charts__group'>
+                <AverageSessionTimeChart sessionsInformation={userAverageSessionTime}/>
+                <ActivityTypeRadarChart activityInformation={userActivityType}/>
+                <AverageScoreRadialBarChart userScore={userPersonalData}/>
+              </div>
+            </section>
+            <section className='main__data__cards'>
+              <Card color={"red"} icon={flame} type={"Calories"} value={userPersonalData.keyData.calorieCount} unitOfMeasure={"kCal"}/>
+              <Card color={"blue"} icon={chicken} type={"Proteines"} value={userPersonalData.keyData.proteinCount} unitOfMeasure={"g"}/>
+              <Card color={"yellow"} icon={apple} type={"Glucides"} value={userPersonalData.keyData.carbohydrateCount} unitOfMeasure={"g"}/>
+              <Card color={"pink"} icon={cheeseburger} type={"Lipides"} value={userPersonalData.keyData.lipidCount} unitOfMeasure={"g"}/>
+            </section>
+          </section>
+        </main>
     </React.Fragment>
   );
 }
